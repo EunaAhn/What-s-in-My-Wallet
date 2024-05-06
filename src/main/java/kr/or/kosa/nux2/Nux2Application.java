01442fcd90919e5994c35e238cf068914ba839e8
hamings <113500975+hamings@users.noreply.github.com>
2024-05-06 17:19:36 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/kr/or/kosa/nux2/Nux2Application.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.nux2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nux2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nux2Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/kosa/nux2/Nux2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.nux2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nux2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

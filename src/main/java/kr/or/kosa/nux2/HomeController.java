845af1d2ea69741734d1b96f2ff450243c7e548b
Hyunwoo Jeong <gusehd3279@gmail.com>
2024-05-07 11:12:48 +0900

    init : 프로젝트 셋팅 (프론트)

---
+++ src/main/java/kr/or/kosa/nux2/HomeController.java
@@ -0,0 +1,59 @@
+package kr.or.kosa.nux2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/onboarding", method= RequestMethod.GET)
+    public String goOnBoarding(HttpServletRequest request) {
+        return "onboarding";
+    }
+
+    @RequestMapping(value = "/login", method= RequestMethod.GET)
+    public String goLogin(HttpServletRequest request) {return "login";}
+
+    @RequestMapping(value = "/signup", method= RequestMethod.GET)
+    public String goSignup(HttpServletRequest request) {
+        return "signup";
+    }
+
+    @RequestMapping(value = "/cardlist", method= RequestMethod.GET)
+    public String goCardlist(HttpServletRequest request) {
+        return "cardlist";
+    }
+
+    @RequestMapping(value = "/history", method= RequestMethod.GET)
+    public String goHistory(HttpServletRequest request) {
+        return "history";
+    }
+
+    @RequestMapping(value = "/analyze", method= RequestMethod.GET)
+    public String goAnalyze(HttpServletRequest request) {
+        return "analyze";
+    }
+
+    @RequestMapping(value = "/suggestion", method= RequestMethod.GET)
+    public String goSuggestion(HttpServletRequest request) {
+        return "suggestion";
+    }
+
+    @RequestMapping(value = "/profile", method= RequestMethod.GET)
+    public String goProfile(HttpServletRequest request) {
+        return "profile";
+    }
+
+    @RequestMapping(value = "/carddetail", method= RequestMethod.GET)
+    public String goCarddetail(HttpServletRequest request) {
+        return "carddetail";
+    }
+
+    @RequestMapping(value = "/cardregistration", method= RequestMethod.GET)
+    public String goCardregistration(HttpServletRequest request) {
+        return "cardregistration";
+    }
+
+}
---
+++ src/main/resources/static/js/sidemenubar.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sideMenus = document.querySelectorAll(".side_menu");
+
+    sideMenus.forEach(function(sideMenu) {
+        const clickedmenu =  document.querySelector(localStorage.getItem("clickedmenu"))
+        clickedmenu.classList.add("clicked_side_menu");
+
+        sideMenus.forEach(function(menu) {
+            if (menu !== clickedmenu && menu.classList.contains("clicked_side_menu")) {
+                menu.classList.remove("clicked_side_menu");
+            }
+        });
+
+        const faSolidIcon = clickedmenu.querySelector(".fa-solid");
+        const sideMenuWord = clickedmenu.querySelector(".side_menu_word");
+
+        faSolidIcon.classList.add("clicked_fa-solid");
+        sideMenuWord.classList.add("clicked_side_menu_word");
+
+        sideMenus.forEach(function(menu) {
+            if (menu !== clickedmenu) {
+                const otherFaSolidIcon = menu.querySelector(".fa-solid");
+                const otherSideMenuWord = menu.querySelector(".side_menu_word");
+                otherFaSolidIcon.classList.remove("clicked_fa-solid");
+                otherSideMenuWord.classList.remove("clicked_side_menu_word");
+            }
+        });
+    });
+});

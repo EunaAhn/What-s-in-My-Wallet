8632bc2e4a45b8c52c8862f416abf2e3880bc570
Ahn Euna <eunaahn@naver.com>
2024-05-10 11:21:43 +0900

    add: carddetail & suggestion & cardregistration page

--- src/main/resources/static/js/suggestion.js
+++ src/main/resources/static/js/suggestion.js
@@ -1,3 +1,96 @@
 document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem('clickedmenu', ".side_suggestion");
 })
+
+
+
+var options_pie = {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+        width: 380,
+        type: 'pie',
+    },
+    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+    responsive: [{
+        breakpoint: 480,
+        options_pie: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var piechart = new ApexCharts(document.querySelector("#pie-chart"), options_pie);
+piechart.render();
+
+
+var options_line = {
+    series: [
+        {
+            name: "Low - 2013",
+            data: [12, 11, 14, 18, 17, 13, 13]
+        }
+    ],
+    chart: {
+        height: 350,
+        type: 'line',
+        dropShadow: {
+            enabled: true,
+            color: '#000',
+            top: 18,
+            left: 7,
+            blur: 10,
+            opacity: 0.2
+        },
+        zoom: {
+            enabled: false
+        },
+        toolbar: {
+            show: false
+        }
+    },
+    colors: ['#77B6EA', '#545454'],
+    dataLabels: {
+        enabled: true,
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    grid: {
+        borderColor: '#e7e7e7',
+        row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+        },
+    },
+    markers: {
+        size: 1
+    },
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        title: {
+            text: 'Month'
+        }
+    },
+    yaxis: {
+        title: {
+            text: 'Temperature'
+        },
+        min: 5,
+        max: 40
+    },
+    legend: {
+        position: 'top',
+        horizontalAlign: 'right',
+        floating: true,
+        offsetY: -25,
+        offsetX: -5
+    }
+};
+
+var linechart = new ApexCharts(document.querySelector("#line-chart"), options_line);
+linechart.render();

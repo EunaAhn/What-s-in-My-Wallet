111562f795538cbf10264b1ad86f5e385721f519
Hyunwoo Jeong <gusehd3279@gmail.com>
2024-05-11 13:19:14 +0900

    feat : cardlist 화면 디자인 수정, api 파일 구조화

--- src/main/java/kr/or/kosa/nux2/web/auth/config/SecurityConfig.java
+++ src/main/java/kr/or/kosa/nux2/web/auth/config/SecurityConfig.java
@@ -54,6 +54,9 @@ public class SecurityConfig {
 //                .formLogin((auth) -> auth.permitAll());
         http
                 .httpBasic((auth) -> auth.disable());
+
+        http
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/test/*").permitAll());
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
--- src/main/resources/static/js/cardlist.js
+++ src/main/resources/static/js/cardlist.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem('clickedmenu', ".side_cardlist");
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cardLists  = document.querySelectorAll(".card-list")
+    cardLists.forEach((cardList) => {
+        cardList.addEventListener("mouseover", () => {
+            const cardTitle = cardList.querySelector(".card-title");
+            cardTitle.style.color = "#9288dd";
+        })
+
+        cardList.addEventListener("mouseout", () => {
+            const cardTitle = cardList.querySelector(".card-title");
+            cardTitle.style.color = "#3b3a45";
+        })
+    })
+})
+

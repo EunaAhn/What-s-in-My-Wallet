220e504af8fa73baa2d6ff9b5edaa1da13a6d49d
hamings <113500975+hamings@users.noreply.github.com>
2024-05-11 14:10:31 +0900

    feat: Role 클래스 추카

---
+++ src/main/java/kr/or/kosa/nux2/domain/member/dto/Role.java
@@ -0,0 +1,17 @@
+package kr.or.kosa.nux2.domain.member.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN,ROLE_USER");
+
+    private final String roles;
+    public static String getIncludingRoles(String role){
+        return Role.valueOf(role).getRoles();
+    }
+
+}

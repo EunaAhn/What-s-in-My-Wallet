d11699167a82c4b0fb964322b34429a9db3680a7
Ahn Euna <eunaahn@naver.com>
2024-05-11 13:20:12 +0900

    add: 소비지도 마커 추가

--- src/main/resources/static/js/cardlist.js
+++ src/main/resources/static/js/cardlist.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem('clickedmenu', ".side_cardlist");
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cardLists = document.querySelectorAll('.card-list');
+
+    cardLists.forEach(function(card) {
+        card.addEventListener('click', function() {
+            // 상세 페이지 URL 생성
+            var cardDetailUrl = 'carddetail';
+
+            // 상세 페이지로 이동
+            window.location.href = cardDetailUrl;
+        });
+    });
+});

db8ccf42d1c57c17dc7a6c642f6ab4f0aac7e85f
hamings <113500975+hamings@users.noreply.github.com>
2024-05-13 17:34:29 +0900

    feat: 관심소비 카테고리 리스트 구현

--- src/main/java/kr/or/kosa/nux2/domain/member/repository/MemberExpenditureCategoryRepository.java
+++ src/main/java/kr/or/kosa/nux2/domain/member/repository/MemberExpenditureCategoryRepository.java
@@ -1,6 +1,7 @@
 package kr.or.kosa.nux2.domain.member.repository;
 
 import kr.or.kosa.nux2.domain.member.dto.MemberConsCategoryDto;
+import kr.or.kosa.nux2.domain.member.dto.MemberDto;
 import kr.or.kosa.nux2.domain.member.mapper.MemberExpenditureCategoryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,10 @@ import java.util.List;
 public class MemberExpenditureCategoryRepository {
     private final MemberExpenditureCategoryMapper memberExpenditureCategoryMapper;
 
-    public int insertMemberConsCategory(List<MemberConsCategoryDto> memberConsCategoryDtoList){
+    public int insertMemberConsCategory(List<MemberConsCategoryDto.MemberConsCategoryRequest> memberConsCategoryDtoList){
         return memberExpenditureCategoryMapper.insertMemberConsCategory(memberConsCategoryDtoList);
+    }
+    public List<MemberConsCategoryDto.MemberConsCategoryResponse> selectMemberConsCategories(MemberDto.MemberIdRequest request){
+        return memberExpenditureCategoryMapper.selectMemberConsCategories(request);
     }
 }

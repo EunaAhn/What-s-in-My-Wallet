c309583757f8d0f40ae9aa02f5d8fd2f9d6a919f
hamings <113500975+hamings@users.noreply.github.com>
2024-05-16 07:22:38 +0900

    refactor: 주석 추가

--- src/main/java/kr/or/kosa/nux2/web/controller/MemberController.java
+++ src/main/java/kr/or/kosa/nux2/web/controller/MemberController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import kr.or.kosa.nux2.domain.member.dto.MemberDto;
 import kr.or.kosa.nux2.domain.member.service.MemberService;
 import kr.or.kosa.nux2.web.auth.principal.CustomUserDetails;
@@ -38,7 +39,7 @@ public class MemberController {
 
     @Operation(summary = "회원가입")
     @PostMapping("/signUp")
-    public String signUp(@RequestBody MemberDto.SignUpRequest request) {
+    public String signUp(@Valid @RequestBody MemberDto.SignUpRequest request) {
         memberService.signUp(request);
         return "cardlist";
     }
--- src/main/java/kr/or/kosa/nux2/web/restcontroller/MemberRestController.java
+++ src/main/java/kr/or/kosa/nux2/web/restcontroller/MemberRestController.java
@@ -2,6 +2,7 @@ package kr.or.kosa.nux2.web.restcontroller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import kr.or.kosa.nux2.domain.member.dto.MemberDto;
 import kr.or.kosa.nux2.domain.member.service.MemberService;
 import kr.or.kosa.nux2.web.common.code.SuccessCode;
@@ -19,13 +20,13 @@ public class MemberRestController {
     private final MemberService memberService;
     @Operation(summary = "인증번호 이메일 발송", description = "회원테이블에 이메일이 있는지 판단하고, 없을 때 인증번호 이메일 발송")
     @PostMapping("/email")
-    public ResponseEntity<ApiResponse<MemberDto.CheckMemberIdResponse>> checkMemberIdAuthentication(@RequestBody MemberDto.MemberIdRequest request) {
+    public ResponseEntity<ApiResponse<MemberDto.CheckMemberIdResponse>> checkMemberIdAuthentication(@Valid @RequestBody MemberDto.MemberIdRequest request) {
         MemberDto.CheckMemberIdResponse response = new MemberDto.CheckMemberIdResponse(memberService.checkMemberId(request));
         return new ResponseEntity<>(new ApiResponse<>(response, SuccessCode.SELECT_SUCCESS), HttpStatus.OK);
     }
     @Operation(summary = "인증번호 검증", description = "이메일에 있는 인증번호와 입력된 값이 같은지 검증")
     @PostMapping("/email/authentication")
-    public ResponseEntity<ApiResponse<MemberDto.CheckAuthenticationNumberResponse>> checkAuthenticationNumber(@RequestBody MemberDto.AuthenticationRequest request) {
+    public ResponseEntity<ApiResponse<MemberDto.CheckAuthenticationNumberResponse>> checkAuthenticationNumber(@Valid @RequestBody MemberDto.AuthenticationRequest request) {
         MemberDto.CheckAuthenticationNumberResponse response = new MemberDto.CheckAuthenticationNumberResponse(memberService.validateAuthenticationNumber(request));
         return new ResponseEntity<>(new ApiResponse<>(response, SuccessCode.SELECT_SUCCESS), HttpStatus.OK);
     }
@@ -37,7 +38,7 @@ public class MemberRestController {
     }
     @Operation(summary = "마이페이지 프로필 수정", description = "회원의 관심카테고리와 월목표지출액 변경")
     @PostMapping("/api/member/profile/edit")
-    public ResponseEntity<ApiResponse<MemberDto.ProfileResponse>> editMemberInfo(@RequestBody MemberDto.UpdateMemberInfoRequest request) {
+    public ResponseEntity<ApiResponse<MemberDto.ProfileResponse>> editMemberInfo(@Valid @RequestBody MemberDto.UpdateMemberInfoRequest request) {
         MemberDto.ProfileResponse response = memberService.updateMemberInfo(request);
         return new ResponseEntity<>(new ApiResponse<>(response, SuccessCode.SELECT_SUCCESS), HttpStatus.OK);
     }
